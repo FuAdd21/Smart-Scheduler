ea099b4b809f990b58d22c06a667c761c381bee5
FuAdd21 <fuadaman19@gmail.com>
2026-01-28 12:37:25 +0300

    Add JWT-protected profile route with auth middleware

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ import authRoutes from './routes/authroutes.js'
 dotenv.config();
 const app = express();
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ strict: false }));
 app.get('/', (req, res) => res.send('API is Running'));
 
 const PORT = process.env.PORT || 5000;
